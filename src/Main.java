934d62be913043a7daf718c664208e899695d8cb
Max Lizunov <lm201111@mail.ru>
2023-12-05 12:26:29 +0400

    Исправил ошибку: "Не выдается ответ для римских чисел при результате больше 10."

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Main {
     public static void main(String[] args) {
@@ -118,7 +117,57 @@ class Main {
     }
 
     private static String convertToRoman(int number) {
-        String[] romanSymbols = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        return romanSymbols[number - 1];
+        if (number <= 0) {
+            throw new IllegalArgumentException("Отрицательные или нулевые значения невозможны для римских чисел");
+        }
+
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+
+        while ((number > 0) && (i < romanNumerals.size())) {
+            RomanNumeral currentSymbol = romanNumerals.get(i);
+            if (currentSymbol.getValue() <= number) {
+                sb.append(currentSymbol.name());
+                number -= currentSymbol.getValue();
+            } else {
+                i++;
+            }
+        }
+
+        return sb.toString();
+    }
+}
+
+enum RomanNumeral {
+    I(1),
+    IV(4),
+    V(5),
+    IX(9),
+    X(10),
+    XL(40),
+    L(50),
+    XC(90),
+    C(100),
+    CD(400),
+    D(500),
+    CM(900),
+    M(1000);
+
+    private final int value;
+
+    RomanNumeral(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static List<RomanNumeral> getReverseSortedValues() {
+        return Arrays.stream(values())
+                .sorted(Comparator.comparing((RomanNumeral e) -> e.value).reversed())
+                .collect(Collectors.toList());
     }
 }
